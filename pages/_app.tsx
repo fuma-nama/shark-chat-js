53dffaa0c6ab6fd1bd59eed9ad4f2b8bb43b1ac8
SonMooSans <god63820869@gmail.com>
2023-02-04 18:34:26 +0800

    Support light dark themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import { client } from "@/gql";
 import "@/styles/globals.css";
 import { ApolloProvider } from "@apollo/client";
+import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <ApolloProvider client={client}>
-            <Component {...pageProps} />
+            <ThemeProvider attribute="class">
+                <Component {...pageProps} />
+            </ThemeProvider>
         </ApolloProvider>
     );
 }
