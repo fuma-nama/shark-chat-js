e01088a119f1287f4cf2536756fef6630f065723
SonMooSans <god63820869@gmail.com>
2023-02-04 23:15:54 +0800

    Add missing Providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,21 @@
 import { client } from "@/gql";
-import "@/styles/globals.css";
 import { ApolloProvider } from "@apollo/client";
 import { ThemeProvider } from "next-themes";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+    Component,
+    pageProps: { session, ...pageProps },
+}: AppProps) {
     return (
-        <ApolloProvider client={client}>
-            <ThemeProvider attribute="class">
-                <Component {...pageProps} />
-            </ThemeProvider>
-        </ApolloProvider>
+        <SessionProvider session={session}>
+            <ApolloProvider client={client}>
+                <ThemeProvider attribute="class" disableTransitionOnChange>
+                    <Component {...pageProps} />
+                </ThemeProvider>
+            </ApolloProvider>
+        </SessionProvider>
     );
 }
