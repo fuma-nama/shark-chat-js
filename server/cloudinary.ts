be530f0b1a6730963dda8b7f56c3a22b3137fd79
SonMooSans <god63820869@gmail.com>
2023-02-11 22:57:40 +0800

    Use Cloudinary for image uploads

---
+++ server/cloudinary.ts
@@ -0,0 +1,12 @@
+import { v2 } from "cloudinary";
+
+const cloudinary = v2;
+
+cloudinary.config({
+    cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+console.log("Connected to Cloudinary!");
+
+export default cloudinary;
