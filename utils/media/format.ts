924d86a9d44d55f43a01470e84c2108201750f20
SonMooSans <god63820869@gmail.com>
2023-02-14 21:10:53 +0800

    Rename .env variable names

--- utils/media/format.ts
+++ utils/media/format.ts
@@ -1,5 +1,5 @@
-const cloudName = process.env.NEXT_PUBLIC_CLOUDINARY_NAME;
+const cloudName = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
 
-export function groupIcon(groupId: string, format = "png", hash: number) {
+export function groupIcon(groupId: string, hash: number, format = "png") {
     return `https://res.cloudinary.com/${cloudName}/image/upload/v${hash}/icons/${groupId}.${format}`;
 }
