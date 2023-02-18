45da0c7b5bd6503b2230085ad6aa93128fde8fc1
SonMooSans <god63820869@gmail.com>
2023-02-18 19:19:20 +0800

    Support fetch group messages

--- server/routers/chat.ts
+++ server/routers/chat.ts
@@ -1,3 +1,5 @@
+import { TRPCError } from "@trpc/server";
+import prisma from "@/prisma/client";
 import { channels } from "@/utils/ably";
 import { z } from "zod";
 import ably from "../ably";
@@ -21,5 +23,40 @@ export const chatRouter = router({
 
                 if (time >= 10) clearInterval(timer);
             }, 1000);
+        }),
+    messages: protectedProcedure
+        .input(
+            z.object({
+                groupId: z.number(),
+                count: z.number().max(50).default(50),
+                after: z.number().optional(),
+                before: z.number().optional(),
+            })
+        )
+        .query(async ({ input, ctx }) => {
+            const userId = ctx.session!!.user.id;
+            const member = prisma.member.findUnique({
+                where: {
+                    group_id_user_id: {
+                        group_id: input.groupId,
+                        user_id: userId,
+                    },
+                },
+            });
+
+            if (member == null) {
+                throw new TRPCError({
+                    message:
+                        "You must join the group in order to receive messages",
+                    code: "BAD_REQUEST",
+                });
+            }
+
+            return prisma.message.findMany({
+                where: {
+                    group_id: input.groupId,
+                },
+                take: Math.min(input.count, 50),
+            });
         }),
 });
