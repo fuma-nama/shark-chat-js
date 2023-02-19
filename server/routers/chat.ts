41761aab2c278f38a8e5422abfab3e396ee7af1d
SonMooSans <god63820869@gmail.com>
2023-02-19 17:21:49 +0800

    Check if user has joined the group

--- server/routers/chat.ts
+++ server/routers/chat.ts
@@ -4,6 +4,7 @@ import { channels } from "@/utils/ably";
 import { z } from "zod";
 import ably from "../ably";
 import { protectedProcedure, router } from "./../trpc";
+import { Session } from "next-auth";
 
 export const chatRouter = router({
     send: protectedProcedure
@@ -14,9 +15,10 @@ export const chatRouter = router({
             })
         )
         .mutation(async ({ input, ctx }) => {
+            await checkIsMemberOf(input.groupId, ctx.session);
+
             const userId = ctx.session.user.id;
             const channel = channels.chat.get(ably);
-
             const message = await prisma.message.create({
                 data: {
                     author_id: userId,
@@ -41,23 +43,7 @@ export const chatRouter = router({
             })
         )
         .query(async ({ input, ctx }) => {
-            const userId = ctx.session!!.user.id;
-            const member = prisma.member.findUnique({
-                where: {
-                    group_id_user_id: {
-                        group_id: input.groupId,
-                        user_id: userId,
-                    },
-                },
-            });
-
-            if (member == null) {
-                throw new TRPCError({
-                    message:
-                        "You must join the group in order to receive messages",
-                    code: "BAD_REQUEST",
-                });
-            }
+            await checkIsMemberOf(input.groupId, ctx.session);
 
             return await prisma.message.findMany({
                 include: {
@@ -73,3 +59,23 @@ export const chatRouter = router({
             });
         }),
 });
+
+async function checkIsMemberOf(group: number, user: Session) {
+    const member = await prisma.member.findUnique({
+        where: {
+            group_id_user_id: {
+                group_id: group,
+                user_id: user.user.id,
+            },
+        },
+    });
+
+    if (member == null) {
+        throw new TRPCError({
+            message: "You must join the group in order to receive messages",
+            code: "BAD_REQUEST",
+        });
+    }
+
+    return member;
+}
