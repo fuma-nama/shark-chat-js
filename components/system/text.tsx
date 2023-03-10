7cf8bb30f00cac5295cde3f14ef46a44bcf72199
SonMooSans <god63820869@gmail.com>
2023-03-10 22:28:34 +0800

    [feat] Create `text` style

--- components/system/text.tsx
+++ components/system/text.tsx
@@ -3,3 +3,17 @@ import { tv } from "tailwind-variants";
 export const label = tv({
     base: "text-[0.875rem] font-medium text-gray-700 dark:text-accent-400",
 });
+
+export const text = tv({
+    variants: {
+        type: {
+            secondary: ["text-accent-800 dark:text-accent-600"],
+            primary: ["text-accent-900 dark:text-accent-50"],
+        },
+        size: {
+            "2xl": ["text-2xl font-bold"],
+            lg: ["text-lg font-semibold"],
+        },
+    },
+    defaultVariants: { type: "secondary" },
+});
