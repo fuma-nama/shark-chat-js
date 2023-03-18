2c7dec64bdcf69cd2e6303dc0b7fce6d056b5f3b
SonMooSans <god63820869@gmail.com>
2023-03-18 19:23:02 +0800

    [fix] Weird input top margin

--- components/input/TextField.tsx
+++ components/input/TextField.tsx
@@ -7,7 +7,7 @@ export type TextFieldProps = ComponentProps<"input"> &
 
 const input = tv({
     base: [
-        "mt-1 block w-full rounded-md px-3 py-2",
+        "block w-full rounded-md px-3 py-2",
         "text-sm text-accent-900 placeholder:text-accent-600 dark:text-accent-400 dark:placeholder:text-accent-700",
         "border border-accent-500 focus-visible:border-transparent dark:border-accent-900 dark:bg-dark-800",
         "focus:outline-none focus-visible:ring-1 focus-visible:ring-brand-500 dark:focus-visible:ring-brand-400 focus-visible:ring-opacity-75",
