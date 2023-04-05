ec1dca818d238467c1b6307c9142a4f188c7dacc
MONEY <76240755+SonMooSans@users.noreply.github.com>
2023-04-06 01:19:44 +0800

    Update MessageItem.tsx

--- components/chat/MessageItem.tsx
+++ components/chat/MessageItem.tsx
@@ -210,7 +210,7 @@ export function Root({
 
 export function Text({ children }: { children: string }) {
     return (
-        <p className="whitespace-pre [word-break:_break-word]">
+        <p className="[word-wrap:break-word] [white-space:break-spaces]">
             <LinkItUrl className="text-brand-500 dark:text-purple-300">
                 {children}
             </LinkItUrl>
