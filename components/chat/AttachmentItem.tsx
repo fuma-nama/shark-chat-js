64b992097f4769f1e3aa94a9fb5349e9405dbc9e
SonMooSans <god63820869@gmail.com>
2023-04-17 19:45:11 +0800

    [fix] Set max width for attachment images

--- components/chat/AttachmentItem.tsx
+++ components/chat/AttachmentItem.tsx
@@ -5,7 +5,7 @@ import { AttachmentType } from "@/server/schema/chat";
 
 export function UploadingAttachmentItem({ file }: { file: File }) {
     return (
-        <div className="p-3 rounded-lg bg-light-100 dark:bg-dark-700 flex flex-col">
+        <div className="p-3 rounded-lg bg-light-100 dark:bg-dark-700 flex flex-col mt-3">
             <p className={text({ size: "md", type: "primary" })}>{file.name}</p>
             <p className={text({ type: "secondary", size: "sm" })}>
                 Uploading...
@@ -14,33 +14,44 @@ export function UploadingAttachmentItem({ file }: { file: File }) {
     );
 }
 
-export function AttachmentItem({ attachment }: { attachment: AttachmentType }) {
-    if (attachment.type === "image") {
-        const cloudinary_prefix =
-            "https://res.cloudinary.com/shark-chat/image/upload/";
+const cloudinary_prefix = "https://res.cloudinary.com/shark-chat/image/upload/";
 
-        if (attachment.url.startsWith(cloudinary_prefix))
-            return (
+export function AttachmentItem({ attachment }: { attachment: AttachmentType }) {
+    if (
+        attachment.type === "image" &&
+        attachment.width != null &&
+        attachment.height != null &&
+        attachment.url.startsWith(cloudinary_prefix)
+    ) {
+        const maxW = Math.min(500, attachment.width);
+        const maxH = Math.min(400, attachment.height);
+        const url = decodeURIComponent(attachment.url).slice(
+            cloudinary_prefix.length
+        );
+
+        return (
+            <div>
                 <div
-                    className="relative max-h-[500px] w-auto"
+                    className="relative w-auto mt-3"
                     style={{
-                        aspectRatio:
-                            (attachment.width ?? 100) /
-                            (attachment.height ?? 100),
+                        aspectRatio: attachment.width / attachment.height,
+                        maxWidth: `${maxW}px`,
+                        maxHeight: `${maxH}px`,
                     }}
                 >
                     <CldImage
                         alt={attachment.name}
-                        src={attachment.url.slice(cloudinary_prefix.length)}
+                        src={url}
                         fill
                         className="rounded-xl"
                     />
                 </div>
-            );
+            </div>
+        );
     }
 
     return (
-        <div className="p-3 rounded-lg bg-light-100 dark:bg-dark-700 flex flex-col">
+        <div className="p-3 rounded-lg bg-light-100 dark:bg-dark-700 flex flex-col mt-3">
             <Link
                 target="_blank"
                 href={attachment.url}
