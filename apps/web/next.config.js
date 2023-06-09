a54fb1efeea507106be8aad6609fd48569e4e96e
SonMooSans <god63820869@gmail.com>
2023-06-10 01:44:09 +0800

    Fix: transpile required packages

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    transpilePackages: ["ably-builder", "db", "server", "shared", "ui"],
     images: {
         domains: ["https://res.cloudinary.com"],
         deviceSizes: [350, 500, 640, 828, 1080, 1200, 1920, 2048, 3840],
