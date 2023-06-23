3e0ea4bc8ef17f750b1ab82b04eea2ff97679736
SonMooSans <god63820869@gmail.com>
2023-06-23 21:53:06 +0800

    Experimental: Virtual Scroll

--- packages/ui/hooks/use-bottom-scroll.ts
+++ packages/ui/hooks/use-bottom-scroll.ts
@@ -1,42 +1,5 @@
 import { useRef, useCallback, useEffect } from "react";
 
 export function useViewScrollController() {
-    const lastScrollDistanceToBottomRef = useRef<number>();
-
-    const resetScroll = useCallback(() => {
-        lastScrollDistanceToBottomRef.current = undefined;
-
-        if (document.scrollingElement) {
-            document.scrollingElement.scrollTop =
-                document.scrollingElement.scrollHeight;
-        }
-    }, []);
-
-    useEffect(() => {
-        const handleRootScroll = () => {
-            const root = document.scrollingElement;
-            if (!root) return;
-
-            const scrollDistanceToBottom = root.scrollHeight - root.scrollTop;
-            lastScrollDistanceToBottomRef.current = scrollDistanceToBottom;
-        };
-
-        document.addEventListener("scroll", handleRootScroll);
-
-        return () => {
-            document.removeEventListener("scroll", handleRootScroll);
-        };
-    }, []);
-
-    const updateScrollPosition = useCallback(() => {
-        const root = document.scrollingElement;
-        const lastScrollDistanceToBottom =
-            lastScrollDistanceToBottomRef.current ?? 0;
-
-        if (root == null) return;
-
-        root.scrollTop = root.scrollHeight - lastScrollDistanceToBottom;
-    }, []);
-
-    return { resetScroll, updateScrollPosition };
+    return {};
 }
