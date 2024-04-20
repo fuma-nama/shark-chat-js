103cc3871701aba33e9eeac1fd27623c6a100eee
Fuma Nama <god63820869@gmail.com>
2024-04-21 00:36:32 +0800

    Fix other problems

--- packages/server/redis/client.ts
+++ packages/server/redis/client.ts
@@ -1,8 +1,8 @@
 import { Redis } from "@upstash/redis";
 
 const redis = new Redis({
-    url: process.env.REDIS_URL,
-    token: process.env.REDIS_TOKEN,
+  url: process.env.REDIS_URL!,
+  token: process.env.REDIS_TOKEN!,
 });
 
 export default redis;
