93b110790efe2e614f5556a3c3e114d1ddd8f4ec
MONEY <god63820869@gmail.com>
2024-05-14 01:32:02 +0000

    Add cropper cas

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import { Provider } from "./layout.client";
+import "cropperjs/dist/cropper.css";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return <Provider>{children}</Provider>;
